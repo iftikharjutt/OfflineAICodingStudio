1b2349123096bacb6b6fa111b26d31fde5160489
iftikharjutt <ilink4ai@gmail.com>
2026-08-13 10:22:35 +0500

    fix: import theme color type

--- core/ui/src/main/java/com/offlineai/core/ui/Theme.kt
+++ core/ui/src/main/java/com/offlineai/core/ui/Theme.kt
@@ -7,7 +7,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
